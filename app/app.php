df8c7e1f7505b6b68dcf2e317608b2564c092574
Mitch Rivet & Don Schemmel <student@epicodus.com>
2015-08-12 14:05:09 -0700

    add routes

--- app/app.php
+++ app/app.php
@@ -9,9 +9,21 @@
 
     $app['debug'] = true;
 
-    $app->register(new Silex\Provider\TwigService(), array(
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
       'twig.path' => __DIR__.'/../views'
     ));
 
     //Routes
-    $app->get()
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('rps_input.html.twig');
+    });
+
+    $app->get("/rps_results", function() use ($app) {
+        $new_game = new RockPaperScissors;
+        $game_result = $new_game->playRockPaperScissors($_GET['first_input'], $_GET['second_input']);
+        return $app ['twig']->render('rps_result.html.twig', array('result' => $game_result));
+    });
+
+    return $app;
+
+?>
